26706e158a43a3777a09239cef84d9ec81329d02
root <root@ideo-aigc.patryk-bartkowiak.comfy>
2024-05-11 10:54:54 +0000

    updated data, dependencies and gitignore

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,3 +3,5 @@ pip install -e ./diffusers
 pip install -r ./diffusers/examples/dreambooth/requirements.txt
 
 accelerate config default
+
+pip install --force-reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
