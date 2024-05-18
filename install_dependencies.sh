0246b2b2949de11ba132e306d7415f041492dfb2
Patryk Bartkowiak <patryk.bartkowiak@tcl.com>
2024-05-18 11:17:53 +0200

    removed unecessary files

--- install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-pip install -e ./diffusers
-
-pip install -r ./diffusers/examples/dreambooth/requirements.txt
-
-accelerate config default
-
-pip install --force-reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
